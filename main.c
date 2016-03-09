5e6401cee53a27d5aa363fba45f78de5709941c4
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-03-09 19:14:30 +0000

    mid changes

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void printMatrix(int** mat, int size){
+	int i, j;
+	for(i = 0; i<size; i++){
+			for(j = 0; j<size; j++)
+					printf("%d ", mat[i][j]);
+			printf("\n");
+	}
+}
 
 int main(int argc, char *argv[]){
-	printf("hello world!\n");
+	int num_v = 0, num_e = 0, i;
+	
+	/* Read the first line of the input feed */
+	scanf("%d %d", &num_v, &num_e);
+	
+	/* Initializes an adjacency matrix populated with zeros */
+	int** adj_matrix = calloc(num_v, sizeof(int));
+	for(i = 0; i < num_v; i++)
+		adj_matrix[i] = calloc(num_v, sizeof(int)); 
+
+	/* */
+	for(i = 0; i < num_e; )
+
+
+	printMatrix(adj_matrix, num_v);
+	
+
 	return 0;
 }	
-
